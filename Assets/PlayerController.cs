aab00ca94a1f2ca999fd5b17b53c72d40cf78f67
Lukas Knoepfel <shylux@gmail.com>
2016-10-06 20:50:07 +0200

    Added hover.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking;
 public class PlayerController : NetworkBehaviour {
 
 	public float speed = 5.0f;
+	public float hover = 10.0f;
 
 	public GameObject bulletPrefab;
 	public Transform bulletSpawn;
@@ -16,11 +17,15 @@ public class PlayerController : NetworkBehaviour {
 
 		var x = Input.GetAxis ("Horizontal");
 		var z = Input.GetAxis ("Vertical");
+		var jump = Input.GetKey (KeyCode.Space);
 		//Vector3 movement = new Vector3 (x, 0, z).normalized * Time.deltaTime * speed;
 		//movement = Quaternion.AngleAxis(transform.rotation.eulerAngles.y, Vector3.up) * movement;
 		//transform.Translate (movement, Space.Self);
 		var _oldPosition = transform.position;
-		transform.Translate((Vector3.forward * z + Vector3.right * x) * Time.deltaTime * speed, Space.Self);
+		var movement = (Vector3.forward * z + Vector3.right * x) * Time.deltaTime * speed;
+		if (jump)
+			movement += Vector3.up * hover * Time.deltaTime;
+		transform.Translate(movement, Space.Self);
 		Debug.DrawLine (_oldPosition, _oldPosition + (transform.position - _oldPosition) * 15);
 
 		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {

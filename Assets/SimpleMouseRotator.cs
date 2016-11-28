ff857bd997c9d974c6176d251b5d76773f15b569
Lukas Knoepfel <shylux@gmail.com>
2016-11-28 21:08:39 +0100

    Cleanup.

--- Assets/SimpleMouseRotator.cs
+++ Assets/SimpleMouseRotator.cs
@@ -15,9 +15,6 @@ public class SimpleMouseRotator: NetworkBehaviour {
     private void FixedUpdate() {
 		if (!isLocalPlayer)
 			return;
-		
-//		if (rot != Quaternion.identity)
-//			return;
 
 		GravityAffected ga = GetComponent<GravityAffected>() as GravityAffected;
 
@@ -30,26 +27,12 @@ public class SimpleMouseRotator: NetworkBehaviour {
 
         // with mouse input, we have direct control with no springback required.
         rotX += inputV*rotationSpeed;
-
 		rotX = Mathf.Clamp(rotX, -90, 90);
 
 		Vector3 lastRotation = head.transform.forward;
 
         // up/down
-		//---- head.transform.localRotation = Quaternion.Lerp(head.transform.localRotation, Quaternion.Euler(-rotX, 0, 0), .1f);
 		head.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
-
-		// propagate left-right rotation to parent
-
-//		Quaternion rotation = Quaternion.Euler (0, rotY, 0);
-//		Quaternion tilt = Quaternion.FromToRotation (Vector3.up, ga.getUpVector ());
-//		Quaternion result = tilt * rotation;
-//		transform.rotation = Quaternion.Slerp (transform.rotation, result, .9f); 
-		//transform.rotation = Quaternion.LookRotation (, ga.getUpVector ());
-		//transform.up = ga.getUpVector();
-		//transform.rotation = Quaternion.AngleAxis(rotY, ga.getUpVector());
-		//Debug.Log (transform.rotation + " " + transform.up);
-//		transform.rotation *= Quaternion.FromToRotation(lastUpVector, ga.getUpVector());
 
 		// left/right
 		transform.Rotate(new Vector3(0, inputH*rotationSpeed, 0), Space.Self);
@@ -58,23 +41,20 @@ public class SimpleMouseRotator: NetworkBehaviour {
 
 		Debug.DrawLine (head.transform.position, head.transform.position + head.transform.forward * 20);
 
+		// Handle up vector change
 		if (lastGravSource != ga.getNearestSource ()) {
+			// set look at with new up vector to calculate new X rotation (up/down)
 			head.transform.LookAt(lastRotation*100000, ga.getUpVector());
+			// project new look vector to X plane and read/set angle
 			Vector3 projectedX = Vector3.ProjectOnPlane(head.transform.forward, Vector3.Cross(head.transform.forward, transform.up));
 			float angleX = Vector3.Angle (projectedX, transform.up);
 			rotX = -angleX + 90;
+			head.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
 
 			transform.rotation = Quaternion.LookRotation(ga.getUpVector(), -lastRotation*100000);
 			transform.Rotate (Vector3.right, 90f);
-//			Vector3 projectedY = Vector3.ProjectOnPlane (head.transform.forward, transform.up);
-//			transform.rotation = Quaternion.LookRotation(ga.getUpVector(), -transform.forward);
-//			transform.Rotate (Vector3.right, 90f);
-//			float rotY = Vector3.Angle (projectedY, head.transform.forward);
-//			transform.Rotate (transform.up, rotY);
 
-			Debug.Log ("rotX: " + rotX + " angle: " + angleX);
 			lastGravSource = ga.getNearestSource ();
 		}
-		//lastUpVector = ga.getUpVector ();
     } 
 }

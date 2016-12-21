82a00e205d77fad91be9e053cd047a9679c2d79d
Timon Tschanz <timon.tschanz@gmail.com>
2016-12-21 10:35:12 +0100

    add basic gunstuff

---
+++ Assets/GunBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunBehaviour : MonoBehaviour {
+	public GameObject BulletPrefab;
+	public float magazine_size;
+
+	private ArrayList bullets;
+	private ArrayList bullets_waiting;
+	// Use this for initialization
+	void Start () {
+		bullets = new ArrayList ();
+		bullets_waiting = new ArrayList ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKeyDown(KeyCode.Mouse1)){
+			if (bullets_waiting.Count > 0){
+				Bullet my_bullet = bullets[0];
+				bullets_waiting.Remove (my_bullet);
+				my_bullet.reset ();
+				bullets.Add (my_bullet);
+			}
+		}
+	}
+}
--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,6 +2,25 @@
 using System.Collections;
 
 public class Bullet : MonoBehaviour {
+	public float start_force;
+	public float propelled_force;
+	public float is_aoe;
+	public int damage;
+	public float damage_range;
+	public GunBehaviour gun_origin;
+	private Vector3 initial_dir;
+	private bool _isactive;
+	private MeshRenderer mesh_renderer;
+	private Rigidbody bullet_rigid;
+
+
+	void Start(){
+		this.transform = gun_origin.transform;
+		mesh_renderer = this.GetComponent("MeshRenderer");
+		bullet_rigid = this.GetComponent ("Rigidbody");
+
+
+	}
 
 	void OnCollisionEnter(Collision collision) {
 		var hit = collision.gameObject;
@@ -9,6 +28,22 @@ public class Bullet : MonoBehaviour {
 		if (health != null) {
 			health.takeDamage (10);
 		}
-		Destroy (gameObject);
+		this._isactive = false;
+		mesh_renderer.enabled (false);
+	}
+
+	void Fire() {
+		initial_dir = gun_origin.transform.eulerAngles.normalized();
+		bullet_rigid.AddForce (start_force * initial_dir);
+	}
+
+	void FixedUpdate() {
+		if (propelled_force > 0) {
+			bullet_rigid.AddRelativeForce (Vector3 (0, propelled_force, 0));					
+	}
+}
+	void reset(){
+		this.transform.position = gun_origin.transform.position();
+			mesh_renderer.enabled(true);
 	}
 }

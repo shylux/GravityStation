5854580f2028e8cbbe4c297c20e5251be510ef3a
Shylux <shylux@gmail.com>
2016-12-29 15:15:37 +0100

    Move scripts. Hide own player. Added crosshair.

---
+++ Assets/Scripts/HideFromLocalPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class HideFromLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) {
+			foreach (Renderer renderer in gameObject.GetComponentsInChildren<Renderer>()) {
+				renderer.enabled = false;
+			}
+			foreach (Canvas canvas in gameObject.GetComponentsInChildren<Canvas>()) {
+				canvas.enabled = false;
+			}
+		}
+	}
+}
